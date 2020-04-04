eb3e47b28c7034a86a46fe5b5482227700c28795
masashi.g <masashi.g@gmail.com>
2020-04-04 12:06:31 +0900

    button event

--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+function App() {
+  function startVideo(e) {
+    e.preventDefault();
+    console.log('start Video');
+  }
+
+  function stopVideo(e) {
+    e.preventDefault();
+    console.log('stop Video');
+  }
+
+  return (
+    <div className="App">
+      App body<br />
+      <button onClick={startVideo}>Start Video</button>
+      <button onClick={stopVideo}>Stop Video</button>
+      <div className="VideoContainer">
+        <video id="local_video">
+        </video>
+      </div>
+    </div>
+  );
+}
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
